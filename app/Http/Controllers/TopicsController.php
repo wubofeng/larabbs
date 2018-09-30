b231b85b40f3a5d15f4cd79dbead2b2400a7ffcb
wubofeng-company <wubofeng@vip.qq.com>
2018-09-30 10:18:10 +0800

    WYSIWYG Editor

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -40,10 +40,11 @@ class TopicsController extends Controller
 		return redirect()->route('topics.show', $topic->id)->with('message', '创建成功');
 	}
 
-	public function edit(Topic $topic)
+	public function edit(Topic $topic, Category $category)
 	{
         $this->authorize('update', $topic);
-		return view('topics.create_and_edit', compact('topic'));
+        $categories = $category->all();
+		return view('topics.create_and_edit', compact('topic', 'categories'));
 	}
 
 	public function update(TopicRequest $request, Topic $topic)
