3731e8f3f311a9778c77c21751042130f2061946
wubofeng-company <wubofeng@vip.qq.com>
2018-10-08 10:00:37 +0800

    Add queue for translate slug

--- app/Jobs/TranslateSlug.php
+++ app/Jobs/TranslateSlug.php
@@ -29,6 +29,6 @@ class TranslateSlug implements ShouldQueue
         $slug = app(SlugTranslateHandler::class)->translate($this->topic->title);
 
         // 为了避免模型监控器死循环调用，我们使用 DB 类直接对数据库进行操作
-        \DB::table('topics')->where('id', 115)->update(['slug' => 123]);
+        \DB::table('topics')->where('id', $this->topic->id)->update(['slug' => $slug]);
     }
 }
