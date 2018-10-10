90661201d17f8a78b3f692cd7182f7a3b82d2cb2
wubofeng-company <wubofeng@vip.qq.com>
2018-10-10 18:04:00 +0800

    Integrated administrator

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -13,8 +13,6 @@ class PagesController extends Controller
 
     public function test()
     {
-        $a = ['a'=>'1', 'b'=>null];
-        $b = ['a'=>'1', 'b'=>'2'];
-        dd($a+$b);
+        dd(config_path('administrator/settings'));
     }
 }
