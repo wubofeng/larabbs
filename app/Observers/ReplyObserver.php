37fccfc2f2de8c215d8b31c91242d17988be1cd4
wubofeng-company <wubofeng@vip.qq.com>
2018-10-10 15:17:52 +0800

    Notify user in email with Queue supported

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -12,7 +12,7 @@ class ReplyObserver
 {
     public function creating(Reply $reply)
     {
-        $reply->content = clean($reply->cont, 'user_topic_body');
+        $reply->content = clean($reply->content, 'user_topic_body');
     }
 
     public function created(Reply $reply)
